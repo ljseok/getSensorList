e01cb129862c73c3038265a59a9c19b1546eac31
ccccljs <cho38suk@hamail.net>
2021-04-06 12:15:26 +0900

    get sensor List

--- app/src/main/java/com/example/getsensorlist/MainActivity.java
+++ app/src/main/java/com/example/getsensorlist/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.getsensorlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mTextSensor;
+    SensorManager sensorMrg;
+    List<Sensor> sensorList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextSensor = findViewById(R.id.textSensor);
+
+        sensorMrg = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMrg.getSensorList(Sensor.TYPE_ALL);
+
+        mTextSensor.append("# Sensors:" + sensorList.size() + ") \n \n" );
+        for(Sensor sensor : sensorList) {
+            mTextSensor.append("Sensor name:" + sensor.getName()  + " \n");
+            mTextSensor.append("Sensor type:" + sensor.getType()  + " \n\n");
+        }
     }
 }
